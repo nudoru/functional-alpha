ffc4b321e78f298fddacf7849b6e02d6331c0b04
nudoru <kheavy@nudoru.com>
2016-12-13 08:40:46 -0500

    packages

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ let util = require('util'),
     Future = require('ramda-fantasy').Future,
     Task = require('data.task');
 
+//------------------------------------------------------------------------------
 // A test promise, if test
+
 const getPromise = (test) => {
  return new Promise((resolve, reject) => {
   if(test) {
@@ -26,6 +28,7 @@ const future = futurizeP(Future);
 const futureizedPromise = future(doSuccessPromise)
 futureizedPromise().fork(e => console.warn(e), d => console.log('Got ', d));
 
+//------------------------------------------------------------------------------
 // First class map
 
 const joeize = (name) => [name,'Joe'].join('-');
@@ -34,3 +37,11 @@ let names = ['Bob','Sarah','Todd'],
     southerize = names.map(joeize);
 
 console.log(southerize);
+
+//------------------------------------------------------------------------------
+// Transduce
+const concat = (acc, x) => acc.concat(x); // Wrap Array.concat
+const mapper = (f, cnct) => (acc, x) => cnct(acc, f(x));
+const filterer = (f, cnct) => (acc, x) => f(x) ? cnct(acc, x) : acc;
+const transRes = [1,2,3].reduce(filterer(x => x > 1, mapper(x => x + 1, concat)), []);
+console.log(transRes);
